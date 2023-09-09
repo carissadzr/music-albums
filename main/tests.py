8a2bfde056805eeb7a4502d0542e11404570b525
Carissa Aida Zahra <carissadzr@gmail.com>
2023-09-09 22:43:15 +0700

    finalized music_albums

--- main/tests.py
+++ main/tests.py
@@ -11,3 +11,8 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+    
+    def test_main_view_no_data(self):
+        response = self.client.get(reverse('show_main'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'No data available')
