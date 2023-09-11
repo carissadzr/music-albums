3cf0b6401c4461c148befa32a03b13cfe1e07605
Carissa Aida Zahra <carissadzr@gmail.com>
2023-09-11 20:48:45 +0700

    added name and class to html page

--- main/views.py
+++ main/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render
 def show_main(request):
     context = {
-        'name': 'Manusia - Tulus',
-        'amount': '10',
-        'description':'This album represents the various dynamics of human emotions, including the spirit of preserving youth, amidst the fluctuations of the heart and feelings, followed by questions about life, self-appreciation, and a variety of emotions in celebrating life'
+        'name': 'Carissa Aida Zahra',
+        'class': 'PBP D'
     }
 
     return render(request, "main.html", context)
